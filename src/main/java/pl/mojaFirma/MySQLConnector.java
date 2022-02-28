440ae4c3a40e2f27f7c88b336f0e4cd7e88f7241
Philo-Tech <marcin.kargol@icloud.com>
2022-02-28 21:05:32 +0100

    Rebuild to Singleton Pattern (and added new Class-> MysSQLConnector)

---
+++ src/main/java/pl/mojaFirma/MySQLConnector.java
@@ -0,0 +1,40 @@
+package pl.mojaFirma;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnector {
+
+    //Ustawienia danych dostepowych do bazy
+    private static final String SQL_LINK = "jdbc:mysql://127.0.0.1:3306/book";
+    private static final String SQL_USER = "root";
+    private static final String SQL_PASS = "";
+    private static final String SQL_CLASS = "com.mysql.cj.jdbc.Driver";
+
+    private Connection connection;
+
+    private static MySQLConnector mySQLConnector = new MySQLConnector();
+
+    public static MySQLConnector getInstance() {
+        return mySQLConnector;
+    }
+
+    private MySQLConnector() {
+        dbConnectionSettings();
+    }
+
+    private void dbConnectionSettings() {
+        try {
+            Class.forName(SQL_CLASS);
+            connection = DriverManager.getConnection(SQL_LINK, SQL_USER, SQL_PASS);
+            System.out.println("Połączono z bazą ");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getMySqlConnection() {
+        return connection;
+    }
+}
